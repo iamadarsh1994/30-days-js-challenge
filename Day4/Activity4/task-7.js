b18a8f3b03bca9312f770847ee593c2950e6d05e
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-07-17 00:31:11 +0530

    Done

--- Day4/Activity4/task-7.js
+++ Day4/Activity4/task-7.js
@@ -1,1 +1,15 @@
 //write a program to print  a pattern using nested for loops
+
+for (let i = 1; i <= 5; i++) { 
+    let row = ''; 
+    for (let j = 1; j <= i; j++) { 
+        row += '* '; 
+    }
+    console.log(row); 
+}
+
+//*
+//* *
+//* * *
+//* * * *
+//* * * * * 
