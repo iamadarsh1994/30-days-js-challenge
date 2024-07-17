2b794e9422767b0e268ba69a4e56e8e815b9e19a
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-07-17 14:09:12 +0530

    Done

--- Day4/Activity3/task-6.js
+++ Day4/Activity3/task-6.js
@@ -1,1 +1,12 @@
 //wap to calculate the factorial of a number using a do...while loop
+//factorial of 5 - 1*2*3*4*5
+let num = 5;
+let factorial = 1;
+let i = num;
+
+do {
+    factorial *= i;
+    i--;
+} while (i > 0);
+
+console.log(`The factorial of ${num} is ${factorial}`);
