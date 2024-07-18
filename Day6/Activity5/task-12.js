50e3fe684e658a0ae9cff92bfd597e2697325a22
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-07-18 23:10:20 +0530

    Done

--- Day6/Activity5/task-12.js
+++ Day6/Activity5/task-12.js
@@ -1,2 +1,9 @@
 //create a two-dimensional array (matrix) and log the entire array to the console
 
+let twoDArray = [
+    [1, 2, 3],  
+    [4, 5, 6],  
+    [7, 8, 9]   
+  ];
+
+  console.log(twoDArray)
--- Day6/Activity5/task-13.js
+++ Day6/Activity5/task-13.js
@@ -1,1 +1,9 @@
 //access and log a specific element from the two-dimensional array.
+let twoDArray = [
+    [1, 2, 3],  
+    [4, 5, 6],  
+    [7, 8, 9]   
+  ];
+
+  console.log(twoDArray[0][0])
+  console.log(twoDArray[1])
