770b6fb3ed6755d0db85dfc6b3dcd5b236c66b7b
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-07-22 00:02:20 +0530

    tasks

--- Day9/tasks.js
+++ Day9/tasks.js
@@ -5,7 +5,9 @@ document.querySelectorAll('.cls1').forEach((element) => {
     element.style.backgroundColor = 'black'; // New color
 });
 //3.create a new 'div' element with some text content and append it to the body.
-document.getElementsByTagName('body').element.innerHTML= '<div>heyyy</div>'
+
+document.getElementsByTagName('body')[0].innerHTML = '<div style=color:black;>heyyy</div>';
+
 //4.create a new li element and add it to an existing ul list
 
 //5.select an HTML element and remove it from the DOM
