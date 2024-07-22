ea22cbc5dfcb391fd21e167b48c69e44a91101ac
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-07-22 20:52:07 +0530

    tasks doing for day10

---
+++ Day10/featurereq.js
@@ -0,0 +1,9 @@
+//1. write a script that adds a click event listener to a button to change the text content of a paragraph.
+
+//2. create a script that handles mouseover and mouseout events to change the background color of an element
+
+//3.write a script that logs key presses and displays input field values using keydown and keyup event listeners
+
+//4. create a script that handles form submission and change events on a select dropdown
+
+//5. write a script that demonstrates event delegation by handling events on dynamically added child elements.
---
+++ Day10/tasks.js
@@ -0,0 +1,42 @@
+//1.add a click event listener to a button that changes the text content of a paragraph. 
+let para = document.getElementById('root')
+document.getElementById('btn').addEventListener('click', function() {
+    para.innerHTML = '<b>Not Now</b>';
+    console.log('this works');
+});
+
+    
+//2.Add a double-click event listener to an image that toggles its visibility.
+document.querySelector('.images').addEventListener('dblclick', function() {
+    const img = document.querySelector('.images');
+    img.style.display = img.style.display === 'none' ? 'block' : 'none';
+});
+
+
+//3. add a mouseover event listener to an element that changes its background color.
+document.getElementById('btn').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'yellow';
+    console.log('this works');
+});
+//4. add a mouseout event listener to an element that resets its background color.
+document.getElementById('btn').addEventListener('mouseout', function() {
+    this.style.backgroundColor = 'pink';
+    console.log('this works');
+});
+//5. add a keydown event listener to an input field that logs the key pressed to the console.
+document.getElementById('inpt').addEventListener('keydown', function(event) {
+    
+    console.log(`Key pressed: ${event.key}`);
+});
+
+//6.add a keyup event listener to an input field that displays the current value in a paragraph.
+
+//7. add a submit event listener to a form that prevents the default submission and logs the form data to the console.
+
+//8. add a change event listener to a select dropdown that displays the selected value in a paragraph 
+
+//9. add a click event listener to a list that logs the next content of the clicked list item using event delegation
+
+//10.add an event listener to a parent element that listens for event from dynamically added child elements.
+
+
