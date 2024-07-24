c31595396e11e7935cd1a36de9e7aed4e1b57146
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-07-25 04:20:43 +0530

    day11

---
+++ Day11/tasks.js
@@ -0,0 +1,4 @@
+//1. create a promise that resolves with a message after a 2-sec timeout and log the message to the console.
+
+//2. create a promise that rejects with an error message after a 2-second timeout and handle the error using '.catch()' .
+
